ecceddaf838eb965b9b492b112cf17208ab91253
Ritesh <113718896+Ritesh-patole@users.noreply.github.com>
2024-02-05 01:40:30 +0530

    Update calc.py
    added some more features

--- calc.py
+++ calc.py
@@ -1,21 +1,22 @@
 # Perform simple arithmetic encoded in an input string:
 # '1 + 2' -> 3, or '1 - 2' -> -1.
-def compute(expression):
-    num0, operator, num1 = expression.split(' ')
-    num0, num1 = float(num0), float(num1)
-    if operator == '+':
-        return num0 + num1
-    elif operator == '-':
-        return num0 - num1
-    elif operator == '*':
-        return num0 * num1
-    elif operator == '/':
-        return num0 / num1
-    else:
-        print('unknown operator!')
-        return None
-        
-       
+print("1. To add\n2. To substract\n3. To Multiply\n4. To divide")
+choice = input("Select operation (1/2/3/4): ")
+if choice in ('1', '2', '3', '4'):
+            num0 = float(input("Enter first number: "))
+            num1 = float(input("Enter second number: "))
+            
+            if choice == '1':
+                print( num0 + num1)
+            elif choice == '2':
+                print( num0 - num1)
+            elif choice == '3':
+                print( num0 * num1)
+            elif choice == '4':
+                print( num0 / num1)
+else:
+    print('unknown operator!')
+    print(None)
 
 
 # this project has been finished!
